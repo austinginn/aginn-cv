ecd67f835837e6bfa32fac22b8c03ee9cd84af59
Austin Ginn <austinleeginn@gmail.com>
2021-07-03 15:33:11 -0400

    Update server.js

--- node/server.js
+++ node/server.js
@@ -2,10 +2,14 @@
 
 //INCLUDES + DIR
 var express = require('express');
+const compression = require('compression'); //gzip
 var http = require('http');
 var _dirname = require('path').dirname(require.main.filename);
 var app = express();
 var server = http.Server(app);
+
+//use
+app.use(compression());
 app.use('/static', express.static(_dirname + "/static"));
 
 ///HTTP client listen on 3000
